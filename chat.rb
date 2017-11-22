d611f87e908a0e901649818992b4ce731af960d0
Leonardo Tolosa <tolosa@gmail.com>
2017-11-22 16:07:47 -0300

    Initial commit

---
+++ chat.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'redis'
+require 'json'
+require 'colorize'
+
+CHANNEL_NAME = 'redis-chat-demo'
+PROMPT = "\r> "
+
+$listener = Redis.new
+$client = Redis.new
+
+puts
+puts 'Redis chat client demo'.upcase.bold
+puts
+
+username = ''
+valid_username = false
+
+until valid_username
+  print 'Enter you username: '
+  username = gets.chomp
+  valid_username = username.length > 0
+  puts 'Invalid username, enter it again' unless valid_username
+end
+
+puts
+puts "Joined chat channel '#{CHANNEL_NAME.bold}' as '#{username.bold}'"
+puts 'Just type and press enter to send a message'
+puts 'Press CTRL + C to exit program'
+puts
+
+Thread.new do
+  $listener.subscribe CHANNEL_NAME do |on|
+    on.message do |channel, message|
+      data = JSON.parse(message)
+      sender = data['username']
+      puts "\r#{sender.bold.red}: #{data['message']}" if sender != username
+      print PROMPT
+    end
+  end
+end
+
+loop do
+  print PROMPT
+  message = gets("\n").chomp
+  $client.publish CHANNEL_NAME, { username: username, message: message }.to_json
+end
